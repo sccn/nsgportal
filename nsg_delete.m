6cc0f9194040de061b2ad45f17f8d0472c2e0e27
ramonmc <ramonmc@ucsd.edu>
2018-12-14 15:48:48 -0800

    nsg username added

--- nsg_delete.m
+++ nsg_delete.m
@@ -2,5 +2,5 @@ function nsg_delete(url)
 
 nsg_info;
 
-command = sprintf('curl -u arnodelorme:%s -H cipres-appkey:%s -X DELETE %s', nsgpassword, nsgkey, url);
+command = sprintf('curl -u %s:%s -H cipres-appkey:%s -X DELETE %s', nsgusername, nsgpassword, nsgkey, url);
 system(command);
