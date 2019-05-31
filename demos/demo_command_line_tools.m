7e4934a4863f10704c82a93d94b42679f96958e2
ramonmc <ram033@eng.ucsd.edu>
2019-05-31 22:54:36 +0200

    Files for demos added to repo

---
+++ demos/demo_command_line_tools.m
@@ -0,0 +1,36 @@
+%% Demo: Creating and managing an NSG job using pop_nsg from the command line
+%
+% Authors: Ramon Martinez-Cancino SCCN/INC/UCSD 2019
+%          Dung Truong            SCCN/INC/UCSD 
+%          Arnaud Delorme         SCCN/INC/UCSD 
+
+%% Running an NSG job from the command line
+% Here we will submit a job located in the path below to NSG. This is
+% included in the plugin folder.
+
+path2zip = '/demo_jobs/TestingEEGLABNSG.zip';
+
+% Runnning a job with default options by passing the path to the job and
+% defining the script to execute inside the job's zip file
+
+[currentjob, alljobs] = pop_nsg('run',path2zip,'filename', 'run_ica_nsg.m');
+
+% Alternatively you can submit a job and provide extra options as 'runtime', 'jobid'.
+% [currentjob1, alljobs] = pop_nsg('run',path2zip, 'jobid', 'runica_testing','runtime', 0.3 ); 
+
+%% Checking job status periodically
+% Check the status of the job periodically by calling the function nsg_recurspol, providing 
+% as argument the NSG job structure, a job ID (see example below) or a job URL
+
+ jobstruct = nsg_recurspoll('runica_testing','pollinterval', 10);  % TODO: add url and struct as input
+
+%% Retrieving job results
+% Retreive and download job results by providing an NSG job structure (see example below),
+% a job ID or a job URL to pop_nsg
+[currentjobout, alljobs] = pop_nsg('output',jobstruct);  % TODO: jobid option as input
+
+%% Deleting an NSG job
+% Delete a job from the NSG record associated with the user NSG credential by
+% providing an NSG job structure (see example below), a job ID or a job URL to pop_nsg
+
+[jobdeleted, alljobs] = pop_nsg('delete',jobstruct);   % TODO: add jobid option as input
