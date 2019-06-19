5eba77fdbfeab2485d7f7f8e79071d559d90ae1e
smakeig <43755798+smakeig@users.noreply.github.com>
2019-06-19 12:02:51 +0200

    Update eegplugin_runicansg.m

--- demos/demo_plugin/runicansg/eegplugin_runicansg.m
+++ demos/demo_plugin/runicansg/eegplugin_runicansg.m
@@ -1,5 +1,5 @@
 function vers = eegplugin_runica_nsg(fig,  try_strings, catch_strings);
 vers = '0.1';
 plotmenu = findobj(fig, 'tag', 'tools');
-submenu = uimenu( plotmenu, 'Label', 'Run ICA NSG', 'separator', 'on', 'callback',...
+submenu = uimenu( plotmenu, 'Label', 'Run ICA via NSG', 'separator', 'on', 'callback',...
     [try_strings.no_check '[EEG]=pop_runicansg(EEG);' catch_strings.add_to_hist]);
