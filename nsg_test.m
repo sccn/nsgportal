31436be55bc0b0036a14591040444b36eb903693
ramonmc <ram033@eng.ucsd.edu>
2019-06-24 16:48:19 +0200

    Updating function for pop_nsg inputs

--- nsg_test.m
+++ nsg_test.m
@@ -1,4 +1,36 @@
-function nsg_test(joblocation)
+% nsg_test() - Run an NSG job test in the local computing resource.
+%
+% Usage: 
+%             >> nsg_test('eeglab/plugins/nsgportal/demos/demo_jobs/TestingEEGLABNSG.zip', 'run_ica_nsg.m')
+%
+% Inputs:
+%   joblocation         - Path to job NSG zip file or folder
+%   joblocation         - Matlab file (.m) to execute during the test
+%
+% Outputs:
+%
+%  See also: 
+%
+% Authors: Arnaud Delorme          SCCN/INC/UCSD 2019
+%          Ramon Martinez-Cancino  SCCN/INC/UCSD 2019
+
+% Copyright (C)  Arnaud Delorme, 2019
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+function nsg_test(joblocation, mfilename, subdirname)
 
 nsg_info;
 
@@ -6,7 +38,7 @@ nsg_info;
 if isempty(ext)
     zipFile = fullfile(outputfolder, [ filename '.zip' ]);
     zip(zipFile, joblocation);
-    nsg_test(zipFile);
+    nsg_test(zipFile, mfilename, subdirname);
 else
     if ~strcmpi(ext, '.zip')
         error('Not a zip file');
@@ -18,12 +50,13 @@ else
         unzip(joblocation);
         
         cd(tmpFolder);
-        testIfTestPresent = dir('test.m');
+        testIfTestPresent = dir(mfilename);
         if isempty(testIfTestPresent)
             zipContent = dir(pwd);
             for iZip = 1:length(zipContent)
                 if isdir(zipContent(iZip).name) && zipContent(iZip).name(1) ~= '.' && zipContent(iZip).name(1) ~= '_'
                     cd(zipContent(iZip).name);
+                    if ~isempty(subdirname), cd(subdirname); end
                     break;
                 end
             end
@@ -32,7 +65,7 @@ else
         disp('NOW RUNNING TEST');
         disp('****************');
         try
-            test
+            run(mfilename);
         catch
             cd(currentFolder);
             error(lasterror);
