37582519205aa2da4e15703cc5326d0e186f0e44
ramonmc <ram033@eng.ucsd.edu>
2020-02-12 13:29:07 -0800

    Comment added for documentation

--- nsg_checknet.m
+++ nsg_checknet.m
@@ -1,6 +1,7 @@
 function status = nsg_checknet()
   status = 0;
   try
+    % If no connection, this will throw an error
     iaddress = java.net.InetAddress.getByName('www.nsgportal.org');
     status = 1;
   end
