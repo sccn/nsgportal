c0b4370c802aaf40e07cf1ea9670acd4325e67e9
ramonmc <ram033@eng.ucsd.edu>
2020-11-11 18:44:51 +0100

    Adding -k parameter to options

--- nsg_jobs.m
+++ nsg_jobs.m
@@ -45,7 +45,7 @@ if ~exist('filename', 'var'), foldname = ''; end
 if nargin < 1
       fprintf('Accessing jobs on NSG...\n')
 
-    command = sprintf('curl -s -u %s:%s -H cipres-appkey:%s "%s/job/%s?expand=true" > tmptxt.xml', nsgusername,  nsgpassword, nsgkey, nsgurl, nsgusername); % request full job status objects
+    command = sprintf('curl -s -k -u %s:%s -H cipres-appkey:%s "%s/job/%s?expand=true" > tmptxt.xml', nsgusername,  nsgpassword, nsgkey, nsgurl, nsgusername); % request full job status objects
     system(command);
     try
         res2 = xml2struct('tmptxt.xml');
@@ -58,7 +58,7 @@ else
     fprintf('Accessing job: "%s" on NSG...\n',jobname)
 
     if strcmpi(filetype, 'xml')
-        command = sprintf(['curl -s -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
+        command = sprintf(['curl -s -k -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
         system(command);
         try
             res2 = xml2struct('tmptxt.xml');
@@ -69,7 +69,7 @@ else
         res2 = removeTextTag(res2);
         
     elseif strcmpi(filetype, 'txt')
-        command = sprintf(['curl -s -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
+        command = sprintf(['curl -s -k -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
         system(command);
         res2 = [ 'tmptxt.' filetype ]; % do not remove txt here. this is the output
     else
@@ -83,7 +83,7 @@ else
         currentFolder = pwd;
         mkdir(tmpFolder)
         cd(tmpFolder);
-        command = sprintf(['curl -s -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
+        command = sprintf(['curl -s -k -u %s:%s -H cipres-appkey:%s "%s?expand=true" > tmptxt.' filetype], nsgusername, nsgpassword, nsgkey, jobname);
         system(command);
         tmp = dir([ 'tmptxt.' filetype ]);
         if tmp.bytes == 0
